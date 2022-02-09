29fc3625a5efec06753b4431b94977e1deed41b1
Konev_Daniil <konev.danKo@yandex.ru>
2022-02-09 18:32:10 +0300

    a.0.0.2

--- Assets/Scripts/Humster/HumsterMover.cs
+++ Assets/Scripts/Humster/HumsterMover.cs
@@ -6,11 +6,13 @@ public class HumsterMover : MonoBehaviour
 {
     [SerializeField] private bool _isRun = true;
     [SerializeField] private float _speed = 1f;
+    [SerializeField] private Rigidbody _rigidbody;
 
     private void Update()
     {
         if(_isRun)
         {
+            /*_rigidbody.AddTorque(Vector3.forward * _speed);*/
             transform.Translate(Vector3.forward * _speed * Time.deltaTime);
         }
     }
