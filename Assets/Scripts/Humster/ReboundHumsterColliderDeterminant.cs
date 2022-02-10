27bfb5a7ce5432ba887deca15ab5846f6e9582f9
Konev_Daniil <konev.danKo@yandex.ru>
2022-02-10 16:59:36 +0300

    a.0.0.3.2

--- Assets/Scripts/Humster/ReboundHumsterColliderDeterminant.cs
+++ Assets/Scripts/Humster/ReboundHumsterColliderDeterminant.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeadHumsterColliderDeterminant : MonoBehaviour
+public class ReboundHumsterColliderDeterminant : MonoBehaviour
 {
     [SerializeField] private HumsterMover _mover;
 
