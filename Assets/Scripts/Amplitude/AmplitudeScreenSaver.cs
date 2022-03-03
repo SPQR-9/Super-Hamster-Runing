0cf0435a687f056f1b15312253c24592fa836a2c
Konev_Daniil <konev.danKo@yandex.ru>
2022-03-03 11:25:09 +0300

    a.0.0.15.1

--- Assets/Scripts/Amplitude/AmplitudeScreenSaver.cs
+++ Assets/Scripts/Amplitude/AmplitudeScreenSaver.cs
@@ -7,7 +7,8 @@ public class AmplitudeScreenSaver : MonoBehaviour
     public void OnGameInitialize(int count)
     {
         Amplitude.Instance.logging = true;
-        Amplitude.Instance.init("05bb8262fd30504c784c86e3c3de99ba");
+        Amplitude.Instance.init("cdd0928f93300d05e4d173bce26f50b9");
+        /*Amplitude.Instance.init("05bb8262fd30504c784c86e3c3de99ba");*/
         Amplitude.Instance.logEvent("game_start", new Dictionary<string, object>()
         {
             {"count",count }
